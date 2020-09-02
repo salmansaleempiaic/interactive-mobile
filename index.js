d6dacb9f52a5ca9b4cca74ab946881a62750c0c9
salmansaleempiaic <61205073+salmansaleempiaic@users.noreply.github.com>
2020-09-03 03:05:18 +0500

    one
    one

--- index.js
+++ index.js
@@ -71,3 +71,65 @@ const currentDay = () => {
 window.setInterval(currentHoursAndMinutes, 1000);
 window.setInterval(currentDate, 1000);
 window.setInterval(currentDay, 1000);
+
+// Elements Capture
+
+const IMG1 = document.getElementById("img1");
+const IMG2 = document.getElementById("img2");
+const IMG3 = document.getElementById("img3");
+const IMG4 = document.getElementById("img4");
+const IMG5 = document.getElementById("img5");
+const IMG6 = document.getElementById("img6");
+const IMG7 = document.getElementById("img7");
+const IMG8 = document.getElementById("img8");
+const IMG9 = document.getElementById("img9");
+const POPUP = document.getElementById("pop-up");
+const SEARCH_BAR = document.getElementById("search-bar");
+const DISPLAY_TIME = document.getElementById("display-time");
+const ICONS = document.getElementById("icons");
+const TEXT = document.getElementById("texts");
+const ICONS1 = document.getElementById("icons-1");
+const TEXT1 = document.getElementById("texts-1");
+const ICONS2 = document.getElementById("icons-2");
+const TEXT2 = document.getElementById("texts-2");
+const SCREEN_BORDER = document.getElementById("screen-border");
+// Pop Up
+
+const popUP = () => {
+  SCREEN_BORDER.style.background = "none";
+  SCREEN_BORDER.style.backgroundColor = "black";
+  POPUP.style.display = "block";
+  SEARCH_BAR.style.display = "none";
+  DISPLAY_TIME.style.display = "none";
+  ICONS.style.display = "none";
+  TEXT.style.display = "none";
+  ICONS1.style.display = "none";
+  TEXT1.style.display = "none";
+  ICONS2.style.display = "none";
+  TEXT2.style.display = "none";
+};
+
+IMG1.addEventListener("click", popUP);
+IMG2.addEventListener("click", popUP);
+IMG3.addEventListener("click", popUP);
+IMG4.addEventListener("click", popUP);
+IMG5.addEventListener("click", popUP);
+IMG6.addEventListener("click", popUP);
+IMG7.addEventListener("click", popUP);
+IMG8.addEventListener("click", popUP);
+IMG9.addEventListener("click", popUP);
+
+const popDOWN = () => {
+  POPUP.style.display = "none";
+  SEARCH_BAR.style.display = "block";
+  DISPLAY_TIME.style.display = "block";
+  ICONS.style.display = "block";
+  TEXT.style.display = "block";
+  ICONS1.style.display = "block";
+  TEXT1.style.display = "block";
+  ICONS2.style.display = "block";
+  TEXT2.style.display = "block";
+  SCREEN_BORDER.style.background = "url('./assets/background.png')";
+  SCREEN_BORDER.style.backgroundSize = "cover";
+};
+POPUP.addEventListener("dblclick", popDOWN);
